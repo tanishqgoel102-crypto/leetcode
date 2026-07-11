7269a807a95beff1c00737ffe0ec5ebefd2eb3f9
Tanishq Goel <tanishqgoel102@gmail.com>
2026-07-11 10:20:42 +0530

    Time: 0 ms (100.00%) | Memory: 23.5 MB (84.22%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -7,7 +7,8 @@ public:
    int count1=0;
    int count2=0;
    for(int i=0;i<n;i++){
-    if(maj1==nums[i]){
+    
+     if(maj1==nums[i]){
         count1++;
     }
     else if(maj2==nums[i]){
@@ -19,8 +20,7 @@ public:
     }
     else if(count2==0){
         maj2=nums[i];
-        count2++;
-    }
+        count2++;}
     else{
         count1--;
         count2--;
@@ -44,5 +44,54 @@ public:
     result.push_back(maj2);
    }
    return result;
+
+
+
+
+
+
+    // int n=nums.size();
+    // int maj1=0;
+    // int maj2=0;
+    // int count1=0;
+    // int count2=0;
+    // for(int i=0;i<n;i++){
+    //     if(count1==0){
+    //         maj1=nums[i];
+    //         count1++;
+    //     }
+    //     else if(count2==0){
+    //         maj2=nums[i];
+    //         count2++;
+    //     }
+    //     else if(nums[i]==maj1){
+    //         count1++;
+    //     }
+    //     else if(nums[i]==maj2){
+    //         count2++;
+    //     }
+    //     else{
+    //         count1--;
+    //         count2--;
+    //     }
+    // }
+    // vector<int> result;
+    // int freq1=0;
+    // int freq2=0;
+    // for(int &num:nums){
+    //     if(num==maj1){
+    //         freq1++;
+    //     }
+    //     else if(num==maj2){
+    //         freq2++;
+    //     }
+    // }
+    // if(freq1>floor(n/3)){
+    //     result.push_back(maj1);
+    // }
+    // if(freq2>floor(n/3)){
+    //     result.push_back(maj2);
+    // }
+    // return result;
     }
 };
