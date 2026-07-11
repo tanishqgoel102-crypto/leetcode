7c82af3505757b4ad97942df00b49f49d70e9311
Tanishq Goel <tanishqgoel102@gmail.com>
2026-07-11 13:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (5.43%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -2,19 +2,32 @@ class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
         int n=mat.size();
-        vector<vector<int>> ans(n,vector<int>(n));
-        int count=0;
-        while(count<4){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans[j][n-i-1]=mat[i][j];
+        // vector<vector<int>> ans(n,vector<int>(n));
+        // int count=0;
+        // while(count<4){
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         ans[j][n-i-1]=mat[i][j];
+        //     }
+        // } 
+        // mat=ans;
+        // count++;
+        // if(ans==target){
+        //     return true;
+        // }
+        // } return false;
+
+
+        for(int count=0;count<4;count++){
+            if(mat==target){
+                return true;
             }
-        } 
-        mat=ans;
-        count++;
-        if(ans==target){
-            return true;
-        }
+            vector<vector<int>> ans(n,vector<int>(n));
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    ans[j][n-1-i]=mat[i][j];
+                }
+            }mat=ans;
         } return false;
     }
 };
