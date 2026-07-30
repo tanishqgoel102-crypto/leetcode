fd9745e5af5dd488f9040f6d9426428612113b1d
Tanishq Goel <tanishqgoel102@gmail.com>
2026-07-30 07:16:32 +0530

    Time: 46 ms (68.21%) | Memory: 29.1 MB (73.60%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -21,40 +21,6 @@ public:
     //     return ans;
 
 
-
-
-    // vector<vector<int>> ans;
-    // sort(nums.begin(),nums.end());
-    // for(int i=0;i<nums.size()-2;i++){
-    //     if(i>0 && nums[i]==nums[i-1]){
-    //         continue;
-    //     }
-    // int left=i+1;
-    // int right=nums.size()-1;
-    // while(left<right){
-    // int sum=nums[i]+nums[left]+nums[right];
-    // if(sum==0){
-    //     ans.push_back({nums[i],nums[left],nums[right]});
-    //     left++;
-    //     right--;
-    //     while(left < right && nums[left] == nums[left - 1]) {
-    //                     left++;
-    //                 }
-
-    //                 while(left < right && nums[right] == nums[right + 1]) {
-    //                     right--;
-    //                 }
-    // }
-    // else if(sum>0){
-    //     right--;
-    // }
-    // else{
-    //     left++;
-    // }}
-    // } return ans;
-
-
-
     // vector<vector<int>> ans;
     // sort(nums.begin(),nums.end());
     // for(int i=0;i<nums.size()-2;i++){
@@ -86,10 +52,10 @@ public:
     //     }}
     // } return ans;
 
-    sort(nums.begin(),nums.end());
     vector<vector<int>> ans;
+    sort(nums.begin(),nums.end());
     for(int i=0;i<nums.size()-2;i++){
-        if(i>0 && nums[i]==nums[i-1]){
+        if(i>0 && nums[i]==nums[i-1] ){
             continue;
         }
         int left=i+1;
@@ -100,24 +66,22 @@ public:
                 ans.push_back({nums[i],nums[left],nums[right]});
                 left++;
                 right--;
-                while(left<right && nums[left]==nums[left-1]){
-                    left++;
-                }
-                while(left<right && nums[right]==nums[right+1]){
-                    right--;
-                }
-            }
-            else if(sum<0){
+            
+            while(left<right && nums[left]==nums[left-1]){
                 left++;
             }
-            else{
+            while(left<right && nums[right]==nums[right+1]){
                 right--;
             }
         }
-
-    }
-return ans;
-
+        else if(sum>0){
+            right--;
+        }
+        else{
+            left++;
+        }
+    } }
+    return ans;
     
     }
 };
