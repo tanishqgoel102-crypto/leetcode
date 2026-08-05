19dcee71f6647173202db4bc09b1f31f2c6badee
Tanishq Goel <tanishqgoel102@gmail.com>
2026-08-05 14:44:49 +0530

    Time: 2 ms (39.69%) | Memory: 10.5 MB (68.28%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+void getAllSubsets(vector<int> &nums,vector<int> &ans,int i,vector<vector<int>>&allSubsets){
+    if(i==nums.size()){
+      allSubsets.push_back({ans});
+      return;
+    }
+    // INCLUDE
+    ans.push_back(nums[i]);
+    getAllSubsets(nums,ans,i+1,allSubsets);
+
+    // BACKTRACKING
+    ans.pop_back();
+
+    // EXCLUDE
+    int idx=i+1;
+    while(idx<nums.size() && nums[idx]==nums[idx-1]){
+        idx++;
+    }
+    getAllSubsets(nums,ans,idx,allSubsets);
+}
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> allSubsets;
+        vector<int> ans;
+        getAllSubsets(nums,ans,0,allSubsets);
+        return allSubsets;
+    }
+};
