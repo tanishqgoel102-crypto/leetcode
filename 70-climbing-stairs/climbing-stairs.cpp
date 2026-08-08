ff3cce27c32e0060ce69586105718a7cb51d3344
Tanishq Goel <tanishqgoel102@gmail.com>
2026-08-08 17:03:53 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (74.62%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+private:
+    int memo[46];
+public:
+    int climbStairs(int n) {
+       
+        if(n==1|| n==2){
+            return n;
+        }
+        if(memo[n]!=0) return memo[n];
+        memo[n]= climbStairs(n-1)+climbStairs(n-2);
+        return memo[n];
+    }
+};
