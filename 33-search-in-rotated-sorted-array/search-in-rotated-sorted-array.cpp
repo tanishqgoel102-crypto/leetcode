3a964ca5cb4a0e949cce9e2f5f294c40cb4a19bf
Tanishq Goel <tanishqgoel102@gmail.com>
2026-08-22 11:36:51 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (69.09%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    int search(vector<int> &nums, int target) {
+    // int s=0;
+    // int e=nums.size()-1;
+    // int mid=s+(e-s)/2;
+    
+    // while(s<=e){
+    //     if(target==nums[mid]){
+    //         return mid;
+            
+    //     }
+    //     else if(nums[s]<=nums[mid]){
+    //         if(nums[s]<=target && target<= nums[mid]){
+    //             e=mid-1;
+    //         }
+    //         else{
+    //             s=mid+1;
+    //         }
+    //     }
+    //     else{
+    //         if(target>=nums[mid] && target<=nums[e]){
+    //             s=mid+1;
+    //         }
+    //         else{
+    //             e=mid-1;
+    //         }
+    //     } mid=s+(e-s)/2;
+    // } return -1;
+
+
+
+    int s=0;
+    int e=nums.size()-1;
+    int mid=s+(e-s)/2;
+    while(s<=e){
+        
+        if(target==nums[mid]){
+            return mid;
+        }
+        else if(nums[s]<=nums[mid]){
+            if(target>=nums[s] && target<=nums[mid]){
+                e=mid-1;
+            }
+            else{
+                s=mid+1;
+            }
+        }
+        else{
+            if(target>=nums[mid] && target<=nums[e]){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        } mid=s+(e-s)/2;
+    }return -1;
+    }
+};
