97f64dace4395ed3d272aa2eff87c958ea4bb07f
Tanishq Goel <tanishqgoel102@gmail.com>
2026-08-23 11:31:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (49.55%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // if(s.length()!=t.length()){
+        //     return false;}
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // return s==t;
+
+        // if(s.length()!=t.length()) {
+        //     return false;}
+        // int freq[26]={0};
+        // for(char c:s){
+        //     freq[c-'a']++;
+        // }
+        // for(char c:t){
+        //     freq[c-'a']--;
+        // }
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]!=0){
+        //         return false;
+        //     }
+        // } return true;
+        
+
+        // if(s.length()!=t.length()){
+        //     return false;}
+        // int freq[26]={0};
+        // for(char c:s) freq[c-'a']++;
+        // for(char c:t) freq[c-'a']--;
+        // for(int i=0;i<26;i++){
+        //     if(freq[i]!=0){
+        //         return false;
+        //     }
+        // } return true;
+
+
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int freq[26]={0};
+        for(char c:s) freq[c-'a']++;
+        for(char c:t) freq[c-'a']--;
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        } return true;
+    }
+};
