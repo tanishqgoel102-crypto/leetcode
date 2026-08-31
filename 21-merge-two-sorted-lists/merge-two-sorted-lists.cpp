afb4db825c362294e449d8b27c8451b52d1fe6ea
Tanishq Goel <tanishqgoel102@gmail.com>
2026-08-31 15:18:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (88.13%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,17 +10,44 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* head1, ListNode* head2) {
-        if(head1==NULL || head2==NULL){
-            return head1==NULL? head2:head1;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        // if(head1==NULL || head2==NULL){
+        //     return head1==NULL?head2:head1;
+        // }
+        // if(head1->val <=head2->val){
+        //     head1->next=mergeTwoLists(head1->next,head2);
+        //     return head1;
+        // }
+        // else{
+        //     head2->next=mergeTwoLists(head1,head2->next);
+        //     return head2;
+        // }
+
+
+
+
+
+        ListNode* dummy=new ListNode(0);
+        ListNode* curr= dummy;
+        while(list1!=NULL && list2!=NULL){
+            if(list1->val <= list2->val){
+                curr->next=list1;
+                list1=list1->next;
+            }
+            else{
+                curr->next=list2;
+                list2=list2->next;
+            }
+            curr=curr->next;
         }
-        if(head1->val <=head2->val){
-            head1->next=mergeTwoLists(head1->next,head2);
-            return head1;
+        if(list1!=NULL){
+            curr->next=list1;
+           
         }
-        else{
-            head2->next=mergeTwoLists(head1,head2->next);
-            return head2;
+        if(list2!=NULL){
+            curr->next=list2;
+            
         }
+        return dummy->next;
     }
 };
