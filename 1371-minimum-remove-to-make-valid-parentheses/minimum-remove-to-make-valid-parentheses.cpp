3ebf655958912058840bbb03d3161e60ca4e0126
Tanishq Goel <tanishqgoel102@gmail.com>
2026-09-12 17:32:52 +0530

    Time: 7 ms (67.46%) | Memory: 14.2 MB (55.96%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.cpp
@@ -1,6 +1,35 @@
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
+        // stack<int>st;
+        // for(int i=0;i<s.size();i++){
+        //     if(s[i]=='('){
+        //         st.push(i);
+        //     }
+        //     else if(!st.empty() && s[i]==')'){
+        //         st.pop();
+        //     }
+        //     else if(st.empty() && s[i]==')'){
+        //         s[i]='#';
+        //     }
+
+        // }
+        // while(!st.empty()){
+        //     s[st.top()]='#';
+        //     st.pop();
+        // }
+        // string ans="";
+        // for(char c:s){
+        //     if(c!='#'){
+        //         ans+=c;
+        //     }
+        // }
+        // return ans;
+
+
+
+
+
         stack<int>st;
         for(int i=0;i<s.size();i++){
             if(s[i]=='('){
@@ -8,11 +37,11 @@ public:
             }
             else if(!st.empty() && s[i]==')'){
                 st.pop();
+
             }
             else if(st.empty() && s[i]==')'){
                 s[i]='#';
             }
-
         }
         while(!st.empty()){
             s[st.top()]='#';
