1eb06b2b4ef775fb8de97e3ac1dd06dbf02b2633
Tanishq Goel <tanishqgoel102@gmail.com>
2026-09-19 15:29:24 +0530

    Time: 16 ms (10.53%) | Memory: 27.9 MB (5.20%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -9,12 +9,24 @@ public:
         // } return nums.size();
 
 
-        int ans=nums.size();
-        for(int i=0;i<nums.size();i++){
-            ans=ans^i;
+        // int ans=nums.size();
+        // for(int i=0;i<nums.size();i++){
+        //     ans=ans^i;
+        // }
+        // for(int i=0;i<nums.size();i++){
+        //     ans=ans^nums[i];
+        // }return ans;
+
+
+        int n=nums.size();
+        unordered_set<int>st;
+        for(int x:nums){
+            st.insert(x);
         }
-        for(int i=0;i<nums.size();i++){
-            ans=ans^nums[i];
-        }return ans;
+        for(int i=0;i<=n;i++){
+            if(st.find(i)==st.end()){
+                return i;
+            }
+        }return -1;
     }
 };
